e3b01bb659ddc3cf9f7ad8bbdee0c45b69adba25
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-07-03 11:41:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-//your JS code here. If required.
-// let button = document.getElementById('reset');
-// let randomsequence=document.getElementById('random-sequence');
-// let arrayOfImages = document.getElementsByClassName('images');
-
-// button.addEventListener('click', newArray);
-
-//  function newArray() {
-//    randomsequence.innerHTML = "";
-	 
-//     let arr = ['img1', 'img2', 'img3', 'img4', 'img5'];
-//     arr.forEach((img)=>{
-//      randomsequence.innerHTML += `<img class="${img}" />`;
-//     })
-//     randomsequence.innerHTML += `<img class="${arr[Math.floor(Math.random()*arr.length)]}"/>`;
-//  }
+your JS code here. If required.
 let button = document.getElementById('reset');
 let randomsequence=document.getElementById('random-sequence');
 let arrayOfImages = document.getElementsByClassName('images');
